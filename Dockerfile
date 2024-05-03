4cb1cedef7625be2aec65ad2a31c97f92d0dd069
Jayanta Dutta <jayanta008@gmail.com>
2024-05-03 22:11:47 +0200

    feat: implemented image uploader service

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+# Stage 1: Backend - Python
+FROM python:3.9-slim AS backend
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Create a non-root user
+RUN useradd -m appuser
+
+WORKDIR /app
+
+# Copy the dependencies file and the application files
+COPY api/requirements.txt .
+COPY api/app.py .
+
+# Install any dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Change the ownership of the working directory to the non-root user
+RUN chown -R appuser:appuser /app
+
+# Create the uploads directory
+RUN mkdir -p uploads
+
+# Change the ownership of the uploads directory to the non-root user
+RUN chown -R appuser:appuser uploads
+
+# Switch to the non-root user
+USER appuser
+
+# Expose port 8080 to the outside world
+EXPOSE 8080
+
+# Stage 2: Frontend - Nginx
+FROM nginx:alpine AS frontend
+
+# Copy the HTML file into the nginx directory
+COPY ui/index.html /usr/share/nginx/html/index.html
+
+# Expose port 80
+EXPOSE 80
+
+# Copy backend files to the nginx directory
+COPY --from=backend /app /app
+
+# Change the ownership of the backend files to the non-root user
+RUN chown -R nginx:nginx /app
+
+# Start Nginx and the Python application
+CMD service nginx start && python /app/app.py
---
+++ api/app.py
@@ -0,0 +1,141 @@
+"""
+Image uploader API:
+1. Upload image to local file mount/postgres database
+2. List all images
+
+Steps:
+1. check allowed extensions
+2. resize_image
+3. upload_image
+4. list image
+"""
+
+import os
+import logging
+import cv2
+from flask import Flask, request, jsonify, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# configure logging
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
+
+ALLOWED_FILE_EXTENSION = {"jpg", "png", "jpeg", "gif", "svg"}
+TARGET_IMAGE_WIDTH = 1500
+app.config["IMAGE_UPLOAD_FOLDER"] = "../uploads"
+
+
+def check_allowed_file(filename):
+    """
+    Check allowed image file extension
+    :param filename:
+    :return:
+    """
+    return filename.rsplit(".", 1)[1].lower() in ALLOWED_FILE_EXTENSION
+
+
+def resize_image(image_path, target_width):
+    """
+    Resize image based on the target_width param maintaining the aspect ratio
+    :param image_path: path to the image file to be resized
+    :param target_width: desired width of resized image
+    :return: None
+    """
+    img = cv2.imread(image_path)
+    if img is None:
+        error_msg = f"image not found or could not be opened. {image_path}"
+        logging.error(error_msg)
+        raise FileNotFoundError(error_msg)
+    # check/calculate aspect ratio
+    height, width = img.shape[:2]
+    aspect_ratio = width / height
+    target_height = int(target_width / aspect_ratio)
+
+    # resize the image
+    resized_image = cv2.resize(img, (target_width, target_height))
+    # save the resized image
+    cv2.imwrite(image_path, resized_image)
+
+
+@app.route("/api/submit", methods=["POST"])
+def upload_image():
+    """
+    Upload image to file store location
+    :return: JSON response indicating success or failure.
+    """
+    if "file" not in request.files:
+        error_msg = "no image file part in request"
+        logging.error(error_msg)
+        return jsonify({"error": {error_msg}}), 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        error_msg = "no image file found"
+        logging.error(error_msg)
+        return jsonify({"error": {error_msg}}), 400
+
+    if file and check_allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config["IMAGE_UPLOAD_FOLDER"], filename)
+        # save the image
+        file.save(file_path)
+        try:
+            # resize image
+            resize_image(file_path, TARGET_IMAGE_WIDTH)
+            return (
+                jsonify({"message": "image uploaded successfully"}),
+                201,
+            )
+        except Exception as e:
+            error_msg = f"Error in resizing image: {str(e)}"
+            logging.error(error_msg)
+            return jsonify({"error": {error_msg}}), 500
+
+    else:
+        return jsonify({"error": "invalid image file"}), 400
+
+
+@app.route("/api/list", methods=["GET"])
+def list_images():
+    """
+    List all images stored in the upload folder alog with their urls
+    :return: JSON response containing a list of image information
+             with image_name and url
+    """
+    image_folder = app.config["IMAGE_UPLOAD_FOLDER"]
+    try:
+        image_files = os.listdir(image_folder)
+
+        # Create a list of dictionaries containing filename and URL
+        images_info = [
+            {
+                "filename": filename,
+                "url": url_for("get_image",
+                               image_name=filename, _external=True),
+            }
+            for filename in image_files
+        ]
+
+    except Exception as e:
+        error_msg = f"error listing images {str(e)}"
+        logging.error(error_msg)
+        return jsonify({"error": {error_msg}}), 500
+
+    return jsonify({"images": images_info}), 200
+
+
+@app.route("/images/<path:image_name>")
+def get_image(image_name):
+    """
+    Retrieve the specified image from the image upload folder
+    :param image_name: name of the image file to retrieve
+    :return: image file to be sent to the client
+    """
+    return send_from_directory(app.config["IMAGE_UPLOAD_FOLDER"], image_name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
